d116a8de8c837927e4958e16a7aa64f16c2d0678
OKisekiO <chudesnayaultika@gmail.com>
2019-05-13 16:53:21 +0200

    CreationDate and LastUpdateDate are strings now

--- src/DTO/JsonResponses/TestSetFolderDTO.cs
+++ src/DTO/JsonResponses/TestSetFolderDTO.cs
@@ -11,12 +11,12 @@ namespace Spira.Client.DTO.JsonResponses
         public int CountNotApplicable { get; set; }
         public int CountNotRun { get; set; }
         public int CountPassed { get; set; }
-        public DateTime CreationDate { get; set; }
+        public string CreationDate { get; set; }
         public object Description { get; set; }
         public object EstimatedDuration { get; set; }
         public object ExecutionDate { get; set; }
         public string IndentLevel { get; set; }
-        public DateTime LastUpdateDate { get; set; }
+        public string LastUpdateDate { get; set; }
         public string Name { get; set; }
         public int? ParentTestSetFolderId { get; set; }
         public int ProjectId { get; set; }
