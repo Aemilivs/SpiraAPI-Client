b8c426b0bfabd5adbdc634b2a7537378e530d6d3
Ravi.Kota <Ravi.Kota@TCG04367>
2023-01-18 13:21:38 +0000

    Added avialable ExecutionStatusID values to the enum

--- src/DTO/JsonResponses/ExecutionStatusIdEnum.cs
+++ src/DTO/JsonResponses/ExecutionStatusIdEnum.cs
@@ -2,8 +2,11 @@
 {
     public enum ExecutionStatusIdEnum
     {
-        NotExecuted = 0,
         Failed = 1,
-        Succeed = 2
+        Succeed = 2,
+        NotRun = 3,
+        NotApplicable = 4,
+        Blocked = 5,
+        Caution = 6
     }
 }
